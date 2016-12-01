58df2c185fff15ab7c56ca44997af485b00d38a9
Victory Osikwemhe <zombieleetnca@gmail.com>
2016-12-01 10:01:37 +0100

    mini library for throwing errors

---
+++ throw.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+throw() {
+    local subComm="${1}"
+    local message="${2}"
+    local open="\e["
+    local close="\e[0m"
+    local bold="1;"
+    local light="0;"
+    local red="31m"
+    local yellow="33m"
+    
+    case $subComm in
+	error)
+
+
+	    [[ -z ${message} ]] && {
+		#${FUNCNAME[$(( ${#FUNCNAME[@]} - $(( ${#FUNCNAME[@]} - 2 )) ))]} ,
+		#  to get the calling function, when you do a - 1 it gives 
+		#   the source builtin command as the calling function,
+		#     so we have to minus by 2
+		message="An Error has occured in ${FUNCNAME[$(( ${#FUNCNAME[@]} - $(( ${#FUNCNAME[@]} - 2 )) ))]}"
+		
+	    }
+	    printf "${open}${bold}${red}%s${close}\n" "${message}"
+	    exit ;
+	    ;;
+	warning)
+	    [[ -z ${message} ]] && {
+		message="Something is wrong at ${FUNCNAME[$(( ${#FUNCNAME[@]} - $(( ${#FUNCNAME[@]} - 2 )) ))]}"
+	    }
+	    printf "${open}${bold}${yellow}%s${close}\n" "${message}"
+	;;
+	*)
+	    ${FUNCNAME} error "Invalid Arugment was passed to ${FUNCNAME}"
+	;;
+    esac
+		      
+}
